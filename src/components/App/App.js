efb6d0cd0a499ba7851b20e49c42cd8e24234875
Rafael Yasis <rafaelyasis@gmail.com>
2023-09-26 22:34:06 -0500

    Added movie details file with working route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import {HashRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import MovieList from '../MovieList/MovieList'
+import MovieDetails from '../MovieDetails/MovieDetails.jsx';
 
 function App() {
   return (
@@ -10,10 +11,12 @@ function App() {
         <Route path="/" exact>
           <MovieList />
         </Route>
-        
         {/* Details page */}
-
+        <Route path="/Movie-Details">
+          <MovieDetails />
+        </Route>
         {/* Add Movie page */}
+
       </Router>
     </div>
   );
---
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -0,0 +1,11 @@
+
+function movieDetails () {
+
+return (
+    <div>
+        <h1>Movie Details go here</h1>
+    </div>
+)
+};
+
+export default movieDetails;
