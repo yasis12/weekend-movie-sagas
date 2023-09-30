fe68f0e477a64f497e920c91335e51c5f7ab4245
Rafael Yasis <rafaelyasis@gmail.com>
2023-09-30 12:13:39 -0500

    Movie detials now works properly

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -4,32 +4,34 @@ import {  useHistory, useParams } from 'react-router-dom';
 
 
 function movieDetails () {
+// React hooks
 const dispatch = useDispatch();
 const history = useHistory();
 const movie = useSelector(store => store.movies);
 const thisID = useParams();
-
+// This part finds what movie has the id that is equal to the useParams ID
 const thisMovie = movie.find(
     (movie) => movie.id === Number(thisID.id)
   );
-console.log("this Movie", thisMovie);
 
+  console.log("THIS MOVIE",thisMovie);
+// Button for routing back to movie List
 const backToMovieList = (event) => {
     history.push('/');
 }
-
+// Need this use effect so we can call the DB for all the movies
 useEffect(() => {
     dispatch({ type: 'FETCH_MOVIES' });
 }, []);
 
-console.log("THE MOVIE IS", movie);
-
 return (
     <div>
-        <h1>Movie Details go here</h1>
         <section className="movies">
-            <h2>{thisMovie.title}</h2>
-     
+            <h1>{thisMovie.title}</h1>
+            <br />
+            <br />
+            <img src={thisMovie.poster} alt={thisMovie.title}/>
+            <h3>{thisMovie.description}</h3>
           
             </section>
         <button onClick={backToMovieList}>Back to Movie List</button>
